3afffaed16a426d63958c5406d92f4aae695f10c
GMosquera1 <genesismosquera@pursuit.org>
2019-09-26 10:34:44 -0400

    initial commit

---
+++ DailyAssistantSE/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DailyAssistantSE
+//
+//  Created by Genesis Mosquera on 9/26/19.
+//  Copyright © 2019 Genesis Mosquera. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
